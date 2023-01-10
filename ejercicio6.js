6dfb80a6e673968867d5f3aa55fb5b227ef58b51
mariatorresroman <116643212+mariatorresroman@users.noreply.github.com>
2023-01-10 13:42:28 +0100

    Create ejercicio6.js

---
+++ ejercicio6.js
@@ -0,0 +1,21 @@
+/***
+ * Recorre la listra de frutas
+ * y verduras utilizando un bucle for
+ * y muestra cada elemento en la consola.
+ */
+
+for(var i=0; i< nombreFRutas.length; i++ ){
+    alert( nombreFrutas[i]);
+}
+
+
+/***
+ * Recorre la listra de frutas
+ * y verduras utilizando un bucle for
+ * y muestra cada elemento en la consola.
+ */
+var i = 0;
+while( i < nombreFrutas.length ){
+
+    alert(nombreFrutas[i]);
+}
