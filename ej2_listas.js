b7e7094b74c217fe41e926e309f495d487f027f7
mariatorresroman <116643212+mariatorresroman@users.noreply.github.com>
2023-01-16 11:09:49 +0100

    Create ej2_listas.js

---
+++ ej2_listas.js
@@ -0,0 +1,10 @@
+
+    // Crea una lista de números impares del 1 al 19 utilizando un bucle for.
+    var numerosImpares=[];
+    for(var index =1;index <=19; index++){
+        if(index % 2==1){
+            numerosImpares.push(index);
+        }
+       
+    }
+    console.log("numeros impares: " + numerosImpares);
