2958914c1e75bcc9b05f1964cbacd64f3bce0438
mariatorresroman <116643212+mariatorresroman@users.noreply.github.com>
2023-01-16 11:08:58 +0100

    Create ej1_listas.js

---
+++ ej1_listas.js
@@ -0,0 +1,9 @@
+// Crea una lista de números pares del 2 al 20 utilizando un bucle while.
+var numeros=[];
+var i=0;
+while(i<=20){
+    numeros.push(i);
+    i = i + 2;
+
+}
+console.log("lista de numeros"+ numeros);
