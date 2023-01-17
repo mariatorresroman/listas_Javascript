10e21990d3b99f70ebfcf8511843df511ea16970
mariatorresroman <116643212+mariatorresroman@users.noreply.github.com>
2023-01-17 13:18:45 +0100

    Create eje4_listas.js

---
+++ eje4_listas.js
@@ -0,0 +1,3 @@
+# listas_Javascript// Agrega un elemento al principio de la lista de comida que creaste en el ejercicio anterior.
+    listas_comida.unshift("papa con carne")
+    console.log(lista_comida)
